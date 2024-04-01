3c21723b71401a55503ec8d9c58297ea2d80502b
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-04-01 12:26:00 +0400

    first commit

---
+++ task.js
@@ -0,0 +1,291 @@
+
+// 1. Obyekt yaradin.
+// let object = {
+//     name: "Turqay",
+//     Surname: "Safarov",
+//     age: 26,
+//     hobbies: ["reading", "swimming", "coding"],
+//     skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//      };
+// console.log(object);
+
+
+// 2. Array icinde coxlu obyektler yaradin
+
+// let students = [
+//     {
+//         name: "samir",
+//         Surname: "Aliyev",
+//         age: 26,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//     },
+//     {
+//         name: "Ali",
+//         surname: "Hasanli",
+//         age: 27,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+
+//     },
+//     {
+//         name: "Orxan",
+//         surname: "Mammadov",
+//         age: 20,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//     },
+//     {
+//         name: "govhar",
+//         surname: "Gahramanova",
+//         age: 21,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//     },
+//     {
+//         name: "ulduz",
+//         surname: "Aliyeva",
+//         age: 22,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//     },
+//     {
+//         name: "Aysu",
+//         surname: "Samadli",
+//         age: 23,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//     },
+//     ]
+// console.log(students);
+
+
+// 3. Telebeler obyektinde bir nece telebe elave edin. Hobbileri, xususiyettleri kimi icerisindede deyerleri qeyd edin.
+// let students = [
+//     {
+//         name: "Turqay",
+//         Surname: "Safarov",
+//         age: 26,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 96,
+//     },
+//     {
+//         name: "Ali",
+//         surname: "Hasanli",
+//         age: 27,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 79,
+//     },
+//     {
+//         name: "Sabir",
+//         surname: "Mammadov",
+//         age: 20,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 98,
+//     },
+//     {
+//         name: "Ayxan",
+//         surname: "Gahramanov",
+//         age: 21,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 90,
+//     },
+//     {
+//         name: "Samir",
+//         surname: "Aliyev",
+//         age: 22,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 85,
+//     },
+//     {
+//         name: "Aysu",
+//         surname: "Samadli",
+//         age: 23,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 92,
+//     },
+//     {
+//         name: "Aynur",
+//         surname: "Hasanli",
+//         age: 24,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 65,
+//     },
+//     {
+//         name: "Arzu",
+//         surname: "Mammadova",
+//         age: 28,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 50,
+//     }
+// ]
+// console.log(students);
+
+
+
+
+
+
+// 4. Students arrayinin qiymet ortalamasini cixardin. (Butun telebelerin qiymetlerini alib sayini bolun.)
+
+// let students = [
+//     {
+//         name: "Turqay",
+//         Surname: "Safarov",
+//         age: 26,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 96,
+//     },
+//     {
+//         name: "Ali",
+//         surname: "Hasanli",
+//         age: 27,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 79,
+//     },
+//     {
+//         name: "Sabir",
+//         surname: "Mammadov",
+//         age: 20,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 98,
+//     },
+//     {
+//         name: "Ayxan",
+//         surname: "Gahramanov",
+//         age: 21,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 90,
+//     },
+//     {
+//         name: "Samir",
+//         surname: "Aliyev",
+//         age: 22,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 85,
+//     },
+//     {
+//         name: "Aysu",
+//         surname: "Samadli",
+//         age: 23,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 92,
+//     },
+//     {
+//         name: "Aynur",
+//         surname: "Hasanli",
+//         age: 24,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 65,
+//     },
+//     {
+//         name: "Arzu",
+//         surname: "Mammadova",
+//         age: 28,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 50,
+//     }
+// ]
+// console.log(students);
+
+// let sum = 0;
+// for (let i = 0; i < students.length; i++) {
+//   sum += students[i].points;
+// }
+// let points_ortalama = sum / students.length;
+// console.log(`Qiymət ortalaması: ${points_ortalama}`)
+
+
+// 5. Qiymeti 90-dan az olan telebeleri Ad ve Soyadi ile birlikde ekrana cixardib onlara xeberdarliq edin.
+
+
+// let students = [
+//     {
+//         name: "Turqay",
+//         Surname: "Safarov",
+//         age: 26,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 96,
+//     },
+//     {
+//         name: "Ali",
+//         surname: "Hasanli",
+//         age: 27,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 79,
+//     },
+//     {
+//         name: "Sabir",
+//         surname: "Mammadov",
+//         age: 20,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 98,
+//     },
+//     {
+//         name: "Ayxan",
+//         surname: "Gahramanov",
+//         age: 21,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 90,
+//     },
+//     {
+//         name: "Samir",
+//         surname: "Aliyev",
+//         age: 22,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 85,
+//     },
+//     {
+//         name: "Aysu",
+//         surname: "Samadli",
+//         age: 23,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 92,
+//     },
+//     {
+//         name: "Aynur",
+//         surname: "Hasanli",
+//         age: 24,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 65,
+//     },
+//     {
+//         name: "Arzu",
+//         surname: "Mammadova",
+//         age: 28,
+//         hobbies: ["reading", "swimming", "coding"],
+//         skills: [" Patience", " Positivity", "  Coaching", "Mediating"],
+//         points: 50,
+//     }
+// ]
+// console.log(students);
+
+// console.log("Qiyməti 90-dan az olan telebələr:");
+// students.forEach(student => {
+//     if (student.points < 90) {
+//         console.log(student.name, student.surname, "- Xəbərdarlıq: Qiymətiniz 90-dan aşağıdır.");
+//     }
+// });
